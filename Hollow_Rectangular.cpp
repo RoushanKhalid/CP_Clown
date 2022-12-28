c99f0262f47298ea4a5672f86c005a0f2c834a70
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2022-12-28 22:37:15 +0600

    Update Hollow_Rectangular.cpp

--- Hollow_Rectangular.cpp
+++ Hollow_Rectangular.cpp
@@ -5,11 +5,11 @@ int main()
     int i,j;
     int m,n;
     cin>>m>>n;
-    for (i = 1; i <= n; i++)
+    for (i = 1; i <= m; i++)
     {
-        for (j = 1; j <= m; j++)
+        for (j = 1; j <= n; j++)
         {
-            if (i == 1 || i == n || j == 1 || j == m) 
+            if (i == 1 || i == m || j == 1 || j == n) 
                 cout << "*";            
             else
                 cout << " ";
