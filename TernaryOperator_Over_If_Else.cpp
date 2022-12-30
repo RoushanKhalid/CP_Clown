bc932eb8ccda42d8f45390b38efb4497e919be8f
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2022-12-30 14:25:52 +0600

    Update TernaryOperator_Over_If_Else.cpp

--- TernaryOperator_Over_If_Else.cpp
+++ TernaryOperator_Over_If_Else.cpp
@@ -5,6 +5,10 @@ int main()
     int marks;
     cout<<"Enter your marks to determine whether you passed or failed :) "<<endl;
     cin>>marks;
+    while(marks>100){
+        cout<<"Please make sure you obtained mark is between 0 to 100."<<endl;
+        cin>>marks;
+    }
     marks>=40 ? cout<<"Passed" : cout<<"Failed";
     return 0;
 }
