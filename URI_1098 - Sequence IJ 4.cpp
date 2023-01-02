7588e9c152b19dd5a0f1cc2cf1626a28e818674f
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-01-02 23:46:32 +0600

    Add files via upload

---
+++ URI_1098 - Sequence IJ 4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double i,j;
+    for(i=0;i<=2;i+=0.2){
+        for(j=1;j<=3;j++){
+            cout<<"I="<<i<<" J="<<j+i<<endl;
+        }
+    }
+    return 0;
+}
