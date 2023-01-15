f14c511beef6976fc1267c1b28c6bbf9e49cfe94
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-01-15 22:59:00 +0600

    Add files via upload

---
+++ CF_236A - Boy or Girl.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    int size=0;
+    sort(str.begin(),str.end());
+    for(int i=0;i<str.size();i++){
+        if(str[i]!=str[i+1]){
+            size++;
+        }
+    }
+    if(size%2==0){
+        cout<<"CHAT WITH HER!"<<endl;
+    }else{
+        cout<<"IGNORE HIM!"<<endl;
+    }
+    return 0;
+}
