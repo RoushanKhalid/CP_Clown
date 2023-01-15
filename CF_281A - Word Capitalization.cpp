a466408eba533a38275968c84165cb6dd023e65d
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-01-15 14:04:08 +0600

    Add files via upload

---
+++ CF_281A - Word Capitalization.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    if(str[0]>=97 && str[122]){
+        str[0]=str[0]-32;
+    }
+    cout<<str<<endl;
+    return 0;
+}
