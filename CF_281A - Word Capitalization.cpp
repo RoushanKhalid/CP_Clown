ee0949b469fac503291f3a218b625851dbae0d85
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-01-15 14:29:06 +0600

    Update CF_281A - Word Capitalization.cpp

--- CF_281A - Word Capitalization.cpp
+++ CF_281A - Word Capitalization.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     string str;
     cin>>str;
-    if(str[0]>=97 && str[122]){
+    if(str[0]>=97 && str[0]<=122){
         str[0]=str[0]-32;
     }
     cout<<str<<endl;
