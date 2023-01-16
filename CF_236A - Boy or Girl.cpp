ec59c5a1d0d1b1c89d928fddd768dde215e43975
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-01-16 10:14:55 +0600

    Update CF_236A - Boy or Girl.cpp

--- CF_236A - Boy or Girl.cpp
+++ CF_236A - Boy or Girl.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
     string str;
     cin>>str;
-    int size=0;
+    int size=str.size();
     sort(str.begin(),str.end());
     for(int i=0;i<str.size();i++){
-        if(str[i]!=str[i+1]){
-            size++;
+        if(str[i]==str[i+1]){
+            size--;
         }
     }
     if(size%2==0){
