1b49e7ae22728306297268cdcff9fc68d55fcf82
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-01-24 16:38:34 +0600

    Add files via upload

---
+++ CF_41A - Translation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str1,str2;
+    cin>>str1>>str2;
+    reverse(str1.begin(),str1.end());
+    if(str1==str2){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;;
+    }
+    return 0;
+}
