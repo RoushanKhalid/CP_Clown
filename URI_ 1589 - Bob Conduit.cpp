0e80edc553af57029021dabd2beff44f0e99043d
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-02-04 20:56:57 +0600

    Add files via upload

---
+++ URI_ 1589 - Bob Conduit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,a,b,sum=0;
+    cin>>n;
+    while(n--){
+        cin>>a>>b;
+        sum=a+b;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
