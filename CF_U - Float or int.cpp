2bc783a0b904b998cef482a054edb03d818aa6a0
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-08-23 21:29:08 +0600

    Add files via upload

---
+++ CF_U - Float or int.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double n; cin>>n;
+    if(n==(int)n){
+        cout<<"int "<<(int)n<<endl;
+    }else{
+        cout<<"float "<<(int)n<<" "<<n-(int)n<<endl;
+    }
+    return 0;
+}
