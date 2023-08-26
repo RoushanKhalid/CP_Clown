e09b54f1d57382a3ae6095eaf9bf2c0a6daf919f
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-08-26 22:53:23 +0600

    Update CF_D - Ali Baba and Puzzles.cpp

--- CF_D - Ali Baba and Puzzles.cpp
+++ CF_D - Ali Baba and Puzzles.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     long long a,b,c,d; cin>>a>>b>>c>>d;
-    if(a+b-c==d || a+b*c==d || a-b+c==d || a-b*c==d || a*b+c==d || a*b-c==d) cout<<"YES";
+    if(a+b-c==d || a+b*c==d || a-b+c==d || a-b*c==d || a*b+c==d || a*b-c==d) cout<<"YES"<<endl;
     else cout<<"NO"<<endl;
     return 0;
 }
