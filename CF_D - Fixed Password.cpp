6e33f268e3863bd18056ea4201b91a405f5bbbff
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-08-28 01:28:58 +0600

    Add files via upload

---
+++ CF_D - Fixed Password.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    while(1){
+        int pass=1999;
+        int test; cin>>test;
+        if(pass==test){
+            cout<<"Correct"<<endl;
+            return 0;
+        }else{
+            cout<<"Wrong"<<endl;
+        }
+    }
+    return 0;
+}
