59215a1a3cdb4c29f8a07fd4fe9801fcf81f678c
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-08-30 01:33:26 +0600

    Update Sieve of Eratosthenes.cpp

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 int main(){
     int num; cin>>num;
-    int arr[num]={0};
-    for(int i=2;i<=num;i++){
+    long long int arr[100000]={0};
+    arr[0]=1;arr[1]=1;
+    for(long long int i=2;i*i<=100000;i++){
         if(arr[i]==0){
-            for(int j=i*i;j<=num;j+=i){
+            for(long long int j=i*i;j<=100000;j+=i){
                 arr[j]=1;
             }
         }
     }
-    for(int i=2;i<=num;i++){
+    for(int i=1;i<=num;i++){
         if(arr[i]==0){
             cout<<i<<" ";
         }
