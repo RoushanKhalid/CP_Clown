3edbfd19bf12bca51e4b9ac3c76f333009f89386
Sk. Roushan Khalid <98089893+RoushanKhalid@users.noreply.github.com>
2023-09-01 15:40:47 +0600

    Add files via upload

---
+++ CF_A - Timon and Pumbaa.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a,b; cin>>a>>b;
+    long long c=a-b;
+    if(c>=0){
+        cout<<c<<endl;
+    }else cout<<"0"<<endl;
+    return 0;
+}
